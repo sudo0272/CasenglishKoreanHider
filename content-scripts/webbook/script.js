3514777d4a322d60a37e43e4831f2554dab9be26
sudo0272 <sudo0272@gmail.com>
2020-02-11 15:42:46 +0900

    removed toggle button because of technical problem and removed everything about toggle button

---
+++ content-scripts/webbook/script.js
@@ -0,0 +1,17 @@
+/*
+    Koreans in list of posts
+        before selecting:
+            patterns: #patterns-menu-init .post-menuitem-trs
+            idioms: #idioms-menu-init .post-menuitem-trs
+
+        after selecting:
+            patterns: #patterns-menu-post .post-menuitem-trs
+            idioms: #idioms-menu-post .post-menuitem-trs
+
+    Koreans in dialogue
+        .post-dialogue h3
+
+    Korean above dialogue
+        #words
+ */
+
